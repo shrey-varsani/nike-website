96265173b8ea09811b587aead7f13f2bf1484699
shrey-varsani <shreyvarsani28@gmail.com>
2026-06-27 17:46:14 +0530

    feat: initialize App component with basic tailwind-styled heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <h1 className='text-5xl text-red-500 font-bold'>hello world</h1>
-    </>
+    <h1 className="text-3xl font-bold underline">hello world</h1>
   )
 }
-
-export default App
