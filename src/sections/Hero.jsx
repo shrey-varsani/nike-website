5b02cf61955892ec32e1f36a4e3151d44289d2c2
shrey-varsani <shreyvarsani28@gmail.com>
2026-06-28 16:50:08 +0530

    feat: implement ShoeCard component and integrate interactive shoe gallery in Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,14 @@
 import Button from "../components/Button";
+import ShoeCard from "../components/ShoeCard";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../assets/constants";
+import { shoes, statistics } from "../assets/constants";
+import { bigShoe1 } from "../assets/images";
+
+import { useState } from "react";
 
 const Hero = () => {
+
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
   return (
     <section
       id="home"
@@ -28,6 +34,30 @@ const Hero = () => {
           </div>
         ))}
       </div>
+      </div>
+
+        {/* now remaining 60% for image */}
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center"> 
+        <img 
+            src={bigShoeImg}  
+            alt="shoe collection" 
+            width={610}
+            height={500}
+            className="object-contain relative z-10"
+        />
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard 
+                imgURL={shoe}
+                changeBigShoeImage=
+                {(shoe) => {setBigShoeImg}}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
 
     </section>
